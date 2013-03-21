5a363f482d7f544f504047e76267f1d08d497198
Kevin Isom <kevin.isom@gmail.com>
2013-03-21 17:37:25 +1300

    initial work

---
+++ public/js/game.js
@@ -0,0 +1,50 @@
+var Game = function (config) {
+    this.dimensions = {
+        width:640, //40 tiles
+        height:480, //30 tiles
+        tile_size: 16
+    }
+}
+var GAME_TYPE = 'Canvas';
+
+Game.prototype.init = function() {
+
+    Crafty.init(600,600);
+    Crafty.canvas.init();
+};
+
+Game.prototype.load = function() {
+    Crafty.load(['/img/block.png', '/img/enemy.png', '/img/player.png', '/img/turret.png'], function () {
+        Crafty.sprite(16, '/img/block.png', {
+            block: [0,0]
+        });
+        Crafty.sprite(16, '/img/enemy.png', {
+            enemy: [0, 0] 
+        });
+        Crafty.sprite(16, '/img/player.png', {
+            player: [0,0] 
+        });
+        Crafty.sprite(16, '/img/turret.png', {
+            turrent: [0,0] 
+        });
+        Crafty.trigger('GameLoaded');
+    });
+};
+Game.prototype.start = function() {
+    
+};
+
+
+
+
+document.addEventListener( "DOMContentLoaded", function(){ 
+    var game = new Game();
+
+    game.init();
+    game.load();
+
+    Crafty.bind('GameLoaded', function(){ 
+        game.start();
+    })
+
+}, false );
